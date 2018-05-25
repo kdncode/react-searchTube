35806540eaebcbf0de787c52045e13be4ff2bfea
kdncode <kdncode@gmail.com>
2018-05-24 23:48:22 -0500

    updated

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class VideoList extends Component {
-    render() {
-        return (
-            <div>
-VideoList
-            </div>
-        );
-    }
+const VideoList = (props) => {
+    const videos = props.videos;
+    const videoItem = Object.keys(videos).map(key => {
+        // console.log(videos[key])
+        const video = videos[key];
+        const imgUrl = video.snippet.thumbnails.default.url;
+        const title = video.snippet.title;
+
+        return <div key={key} onClick={ () => props.selectItem(video) }>
+            <figure>
+                <img src={imgUrl} alt="" />
+                <figcaption>{title}</figcaption>
+            </figure>
+        </div>
+    });
+    return videoItem;
 }
 
 export default VideoList;
--- src/components/VideoPlay.js
+++ src/components/VideoPlay.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class VideoPlay extends Component {
-    render() {
-        return (
-            <div>
-                VideoPlay
+const VideoPlay = (props) => {
+    const { id, snippet } = props.video;
+    const videoId = id.videoId;
+    const url = `https://www.youtube.com/embed/${videoId}`;
+    return (
+        <div className="video-container">
+            <iframe title="video_yt" src={url} width="700" height="350" />
+            <div className="desc">
+                <p><b>{snippet.title}</b></p>
+                <p>{snippet.description}</p>
+                <div>Publish at: {snippet.publishedAt}</div>
             </div>
-        );
-    }
+        </div>
+    )
 }
 
 export default VideoPlay;
